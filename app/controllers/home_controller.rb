0683e8fe3745795f490b3fec05ec4a4f422ff31f
Austin Pierce <austinpierce32@gmail.com>
2022-02-24 20:20:59 -0500

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+end
+# letters = ["a", "b", "c", "d"]
+# puts letters[0]
+# puts letters[1]
+
+# def print_array(arr)
+#   i = 0
+#   while i < arr.length
+#     puts arr[i]
+#     i += 1
+#   end
+# end
+# food = ['pizza','sushi', 'burgers', 'fries', 'ramen']
+# peeps = ["Salina", "Austin", "Lee"]
+# print_array(peeps)
+# print_array(food)
